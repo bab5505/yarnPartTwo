4ce1cde97e86424dce0a244058be4a75a402412e
bab5505 <robertarthurm@gmail.com>
2024-09-24 11:53:25 -0700

    Api update

--- src/ProgressTracker.js
+++ src/ProgressTracker.js
@@ -1,7 +1,9 @@
 import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'https://yarnparttwo.onrender.com';
+const API_BASE_URL = window.location.hostname === 'localhost'
+  ? 'http://localhost:3000'
+  : 'https://yarnparttwo.onrender.com';
 
 const ProgressTracker = () => {
   const [input, setInput] = useState('');
